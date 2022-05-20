aceffe2934a54cffb87850cca8f4cf16df4ea523
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-05-20 09:27:08 -0400

    modificacion tabla

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
 &nbsp;
 <div class="container">
     <div class="card-pannel grey lighten-5">
-        <table class="table-responsive table-stripped">
+        <table class="striped centered table-responsive">
             <thead>
             <tr>
-                <th>Name</th>
-                <th>Item Name</th>
-                <th>Item Price</th>
+                <th>Producto</th>
+                <th>Tipo</th>
+                <th>Precio Neto</th>
+                <th>Flete</th>
+                <th>Seguro</th>
+                <th>Ad valorem</th>
+                <th>I.V.A.</th>
+                <th>Total</th>
                 <th>Acciones</th>
             </tr>
             </thead>
@@ -20,6 +25,23 @@
                 <td>Alvin</td>
                 <td>Eclair</td>
                 <td>$0.87</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><a href=""><i class="material-icons blue-text">edit</i></a>
+                    <a href=""><i class="material-icons red-text">delete</i></a></td>
+            </tr>
+            <tr>
+                <td>Alvin</td>
+                <td>Eclair</td>
+                <td>$0.87</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
                 <td><a href=""><i class="material-icons blue-text">edit</i></a>
                     <a href=""><i class="material-icons red-text">delete</i></a></td>
             </tr>
